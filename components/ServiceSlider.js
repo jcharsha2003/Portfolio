99c4ba5a1c4a37b8557d5b1585916e1e8ec588bd
Chandu <jcharsha2003@gmail.com>
2023-10-01 18:07:25 +0530

    Portfolio_v1

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,34 +1,96 @@
-// data
-const serviceData = [
+//import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// icons
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxArrowTopRight,
+} from "react-icons/rx";
+
+// import required modules
+import { FreeMode, Pagination } from "swiper";
+
+// service data
+export const serviceDate = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Branding",
+    description: "loream ipsum delor sit amet,consectetur",
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Design",
+    description: "loream ipsum delor sit amet,consectetur",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description: "loream ipsum delor sit amet,consectetur",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Copywriting",
+    description: "loream ipsum delor sit amet,consectetur",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "SEO",
+    description: "loream ipsum delor sit amet,consectetur",
   },
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      modules={[FreeMode, Pagination]}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      className="h-[240px] sm:h-[340px] "
+    >
+      {serviceDate.map((item, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="bg-[rgba(64,47,123,0.15)] h-max rounde-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+              {/* icon */}
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              {/* title & desc */}
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal">
+                  {item.description}
+                </p>
+              </div>
+              {/* arrow */}
+              <div className="text-3xl">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300 " />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,35 @@
+import Link from "next/link";
+
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiLinkedinBoxLine,
+  RiDribbbleLine,
+  RiBehanceLine,
+  RiPinterestLine,
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={"https://www.instagram.com/j.chandraharsha/"} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine/>
+      </Link>
+      <Link href={"https://www.youtube.com/@chandraharshajalakam8809/videos"} className="hover:text-accent transition-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={"https://www.facebook.com/profile.php?id=100093512545303"} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+      <Link href={"https://www.linkedin.com/in/chandra-harsha-jalakam-12ba01244/"} className="hover:text-accent transition-all duration-300">
+        <RiLinkedinBoxLine />
+      </Link>
+      <Link href={"https://in.pinterest.com/chandraharshaj/"} className="hover:text-accent transition-all duration-300">
+        <RiPinterestLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
